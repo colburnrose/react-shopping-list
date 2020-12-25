e159dc0a86bfd48b5cba57b6ca38ee1d1a3178b2
colburncodes <colburnsanders@gmail.com>
2020-12-24 19:27:10 -0800

    callback props

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -5,7 +5,12 @@ export default function ShoppingList(props) {
   return (
     <ul>
       {props.items.map((item, i) => (
-        <ShoppingItem key={i} item={item} />
+        <ShoppingItem
+          key={i}
+          item={item}
+          onDeleteItem={props.onDeleteItem}
+          onCheckItem={props.onCheckItem}
+        />
       ))}
     </ul>
   );
